f9fd1aebf9c49a1377ba3283da4e72577cfb27e2
francoromeo <francoromeo5012@gmail.com>
2023-09-16 22:48:16 -0300

    swiper image

--- app.js
+++ app.js
@@ -11,10 +11,20 @@ textButtons.forEach(textButton => {
 })
 
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
+    slidesPerView: 1,
+    spaceBetween: 20,
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
     },
+    breakpoints: {
+        599: {
+            slidesPerView: 2,
+            spaceBetween: 40
+        },
+        1023: {
+            slidesPerView: 3,
+            spaceBetween: 60
+        }
+    }
 });
