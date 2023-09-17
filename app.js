a9edffa7849fa8191e7e1d2284db90bf4dae0d61
francoromeo <francoromeo5012@gmail.com>
2023-09-16 21:13:04 -0300

    about

--- app.js
+++ app.js
@@ -7,5 +7,5 @@ const textButtons = document.querySelectorAll('.contact_btn');
 textButtons.forEach(textButton => {
     let text = textButton.querySelector('p');
 
-    text.innerHTML = text.innerHTML.split('').map((character, index) => `<span style="transform: rotate(${index * 12}deg)">${character}</span>`).join()
+    text.innerHTML = text.innerHTML.split('').map((character, index) => `<span style="transform: rotate(${index * 12}deg)">${character}</span>`).join('')
 })
