b0c3599c3c1c6e2991012aef39dd2cf5f5b221a1
francoromeo <francoromeo5012@gmail.com>
2023-09-17 20:51:17 -0300

    animatiosn close open

--- app.js
+++ app.js
@@ -28,3 +28,25 @@ var swiper = new Swiper(".mySwiper", {
         }
     }
 });
+
+const nav = document.querySelector('.nav_links');
+const openNavBtn = document.querySelector('#nav_toggle-open');
+const closeNavBtn = document.querySelector('#nav_toggle-close');
+
+const openNav = () =>{
+    nav.style.display = 'flex';
+    openNavBtn.style.display = 'none';
+    closeNavBtn.style.display = 'inline-block';
+}
+openNavBtn.addEventListener('click', openNav);
+
+const closeNav = () =>{
+    nav.style.display = 'none';
+    openNavBtn.style.display = 'inline-block';
+    closeNavBtn.style.display = 'none';
+}
+closeNavBtn.addEventListener('click', closeNav)
+
+nav.querySelectorAll('li a').forEach(navLink => {
+    navLink.addEventListener('click', closeNav);
+})
